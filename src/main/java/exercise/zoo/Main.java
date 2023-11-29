7fda19efaefb44597d7e6609cca21235c004bec8
Giacomo UGOLINI <giacomo.ugolini@alten.it>
2023-11-29 16:57:24 +0100

    test: change test by introducing optional control

--- src/main/java/exercise/zoo/Main.java
+++ src/main/java/exercise/zoo/Main.java
@@ -16,27 +16,30 @@ public class Main {
 
         logger.debug("Welcome, let's test!");
         ZooManager zoo = new ZooManager();
-        zoo.addAnimal(new Eagle("Aquila1", "abc", 3, LocalDate.now(), 25, 100, 100));
-        zoo.addAnimal(new Eagle("Aquila2", "cvb", 5, LocalDate.now(), 30, 110, 150));
-        zoo.addAnimal(new Lion("Leone1", "abc", 3, LocalDate.now(), 150, 124, 70));
-        zoo.addAnimal(new Lion("Leone2", "cvb", 5, LocalDate.now(), 160, 136, 59));
-        zoo.addAnimal(new Tiger("Tigre1", "abc", 3, LocalDate.now(), 120, 111, 32));
-        zoo.addAnimal(new Tiger("Tigre2", "cvb", 5, LocalDate.now(), 140, 124, 43));
-        logger.debug("Highest Lion: {}", zoo.getHighestSpecimen(Lion.class).getName());
-        logger.debug("Lowest Lion: {}", zoo.getLowestSpecimen(Lion.class).getName());
-        Lion a = zoo.getHighestSpecimen(Lion.class);
-        logger.debug("Highest Tiger: {}", a.getName());
-        logger.debug("Lowest Tiger: {}", zoo.getLowestSpecimen(Tiger.class).getName());
-        logger.debug("Highest Eagle: {}", zoo.getHighestSpecimen(Eagle.class).getName());
-        logger.debug("Lowest Eagle: {}", zoo.getLowestSpecimen(Eagle.class).getName());
-        logger.debug("Heavier Lion: {}", zoo.getHeaviestSpecimen(Lion.class).getName());
-        logger.debug("Lighter Lion: {}", zoo.getLightestSpecimen(Lion.class).getName());
-        logger.debug("Heavier Tiger: {}", zoo.getHeaviestSpecimen(Tiger.class).getName());
-        logger.debug("Lighter Tiger: {}", zoo.getLightestSpecimen(Tiger.class).getName());
-        logger.debug("Heavier Eagle: {}", zoo.getHeaviestSpecimen(Eagle.class).getName());
-        logger.debug("Lighter Eagle: {}", zoo.getLightestSpecimen(Eagle.class).getName());
-        logger.debug("Longest Tailed Animal: {}", zoo.getLongestTailedAnimal().getName());
-        logger.debug("Largest Wingspan Animal: {}", zoo.getLargestWingspanAnimal().getName());
+        zoo.addAnimal(new Eagle("Eagle1", "abc", 3, LocalDate.now(), 25, 100, 100));
+        zoo.addAnimal(new Eagle("Eagle2", "cvb", 5, LocalDate.now(), 30, 110, 150));
+        zoo.addAnimal(new Lion("Lion1", "abc", 3, LocalDate.now(), 150, 124, 70));
+        zoo.addAnimal(new Lion("Lion2", "cvb", 5, LocalDate.now(), 160, 136, 59));
+        zoo.addAnimal(new Tiger("Tiger1", "abc", 3, LocalDate.now(), 120, 111, 32));
+        zoo.addAnimal(new Tiger("Tiger2", "cvb", 5, LocalDate.now(), 140, 124, 43));
+        zoo.getHighestSpecimen(Lion.class).ifPresentOrElse(
+                lion -> logger.debug("Highest Lion : {}", lion.getName()),
+                () -> logger.debug("No lions in the zoo."));
+        zoo.getLowestSpecimen(Lion.class).ifPresentOrElse(
+                lion -> logger.debug("Lowest Lion : {}", lion.getName()),
+                () -> logger.debug("No lions in the zoo."));
+        zoo.getHeaviestSpecimen(Eagle.class).ifPresentOrElse(
+                eagle -> logger.debug("Heaviest Eagle : {}", eagle.getName()),
+                () -> logger.debug("No eagles in the zoo."));
+        zoo.getLightestSpecimen(Tiger.class).ifPresentOrElse(
+                tiger -> logger.debug("Heaviest Eagle : {}", tiger.getName()),
+                () -> logger.debug("No eagles in the zoo."));
+        zoo.getLongestTailedAnimal().ifPresentOrElse(
+                tailedAnimal -> logger.debug("Longest Tailed Animal : {}", tailedAnimal.getName()),
+                () -> logger.debug("No tailed animals in the zoo."));
+        zoo.getLargestWingspanAnimal().ifPresentOrElse(
+                wingedAnimal -> logger.debug("Largest Wingspan Animal : {}", wingedAnimal.getName()),
+                () -> logger.debug("No winged animal in the zoo."));
         logger.debug("Test finished!");
     }
 }
